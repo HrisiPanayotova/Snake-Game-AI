8e4e1f767d5af4bf6c123c133622873b0acc793a
Hristiyana Panayotova <hpanayotova@vmware.com>
2021-02-14 00:41:50 +0200

    adjust geometry helper

--- js/GeometryHelpers.js
+++ js/GeometryHelpers.js
@@ -1,76 +1,78 @@
 function getLineByPoints(point1, point2) {
-    let { col: col1, row: row1 } = point1;
-    let { col: col2, row: row2 } = point2;
-    let a = (row2 - row1) / (col2 - col1);
-    let b = row1 - a * col1;
-    return [a, b];
+    let { x: x1, y: y1 } = point1;
+    let { x: x2, y: y2 } = point2;
+    let a = (y2 - y1) / (x2 - x1);
+    let b = y1 - a * x1;
+    return { a, b };
 }
 
 function isAboveLine(point, line) {
-    let lineValue = line.a * point.col + line.b;
-    return point.row > lineValue;
+    let lineValue = line.a * point.x + line.b;
+    return point.y < lineValue;
 }
 
 function isBelowLine(point, line) {
-    let lineValue = line.a * point.col + line.b;
-    return point.row < lineValue;
+    let lineValue = line.a * point.x + line.b;
+    return point.y > lineValue;
 }
 
 function isAtLine(point, line) {
-    let lineValue = line.a * point.col + line.b;
-    return point.row = lineValue;
+    let lineValue = line.a * point.x + line.b;
+    return point.y == lineValue;
 }
 
-const partion0Filter = (centerPoint, point) => point.col <= centerPoint.col && isAboveLine(point,
-    getLineByPoints(centerPoint, { col: centerPoint.col + 1 / 2, row: centerPoint.row + 1 / 2 }));
+export const partion0Filter = (centerPoint, point) => {
+    return point.x >= centerPoint.x && isAboveLine(point,
+        getLineByPoints(centerPoint, { x: centerPoint.x + 1 / 2, y: centerPoint.y - 1 / 2 }))
+};
 
-const partion1Filter = (centerPoint, point) => {
-    let line = getLineByPoints(centerPoint, { col: centerPoint.col + 1 / 2, row: centerPoint.row + 1 / 2 });
-    return point.row > centerPoint.row && (isBelowLine(point, line) || isAtLine(point, line));
+export const partion1Filter = (centerPoint, point) => {
+    let line = getLineByPoints(centerPoint, { x: centerPoint.x + 1 / 2, y: centerPoint.y - 1 / 2 });
+    return point.y < centerPoint.y && (isBelowLine(point, line) || isAtLine(point, line));
 }
 
-const partion2Filter = (centerPoint, point) => point.row <= centerPoint.row && isAboveLine(point,
-    getLineByPoints(centerPoint, { col: centerPoint.col + 1 / 2, row: centerPoint.row - 1 / 2 }));
+export const partion2Filter = (centerPoint, point) => point.y >= centerPoint.y && isAboveLine(point,
+    getLineByPoints(centerPoint, { x: centerPoint.x + 1 / 2, y: centerPoint.y + 1 / 2 }));
 
-const partion3Filter = (centerPoint, point) => {
-    let line = getLineByPoints(centerPoint, { col: centerPoint.col + 1 / 2, row: centerPoint.row - 1 / 2 });
-    return point.row > centerPoint.row && (isBelowLine(point, line) || isAtLine(point, line));
+export const partion3Filter = (centerPoint, point) => {
+    let line = getLineByPoints(centerPoint, { x: centerPoint.x + 1 / 2, y: centerPoint.y + 1 / 2 });
+    return point.x > centerPoint.x && (isBelowLine(point, line) || isAtLine(point, line));
 }
 
-const partion4Filter = (centerPoint, point) => point.col >= centerPoint.col && isBelowLine(point,
-    getLineByPoints(centerPoint, { col: centerPoint.col - 1 / 2, row: centerPoint.row - 1 / 2 }));
+export const partion4Filter = (centerPoint, point) => point.x <= centerPoint.x && isBelowLine(point,
+    getLineByPoints(centerPoint, { x: centerPoint.x - 1 / 2, y: centerPoint.y + 1 / 2 }));
 
 
-const partion5Filter = (centerPoint, point) => {
-    let line = getLineByPoints(centerPoint, { col: centerPoint.col - 1 / 2, row: centerPoint.row - 1 / 2 });
-    return point.row < centerPoint.row && (isAboveLine(point, line) || isAtLine(point, line));
+export const partion5Filter = (centerPoint, point) => {
+    let line = getLineByPoints(centerPoint, { x: centerPoint.x - 1 / 2, y: centerPoint.y + 1 / 2 });
+    return point.y > centerPoint.y && (isAboveLine(point, line) || isAtLine(point, line));
 }
 
-const partion6Filter = (centerPoint, point) => point.col >= centerPoint.col && isBelowLine(point,
-    getLineByPoints(centerPoint, { col: centerPoint.col - 1 / 2, row: centerPoint.row + 1 / 2 }));
+export const partion6Filter = (centerPoint, point) => point.y <= centerPoint.y && isBelowLine(point,
+    getLineByPoints(centerPoint, { x: centerPoint.x - 1 / 2, y: centerPoint.y - 1 / 2 }));
 
-const partion7Filter = (centerPoint, point) => {
-    let line = getLineByPoints(centerPoint, { col: centerPoint.col - 1 / 2, row: centerPoint.row + 1 / 2 });
-    return point.col > centerPoint.col && (isAboveLine(point, line) || isAtLine(point, line));
+export const partion7Filter = (centerPoint, point) => {
+    let line = getLineByPoints(centerPoint, { x: centerPoint.x - 1 / 2, y: centerPoint.y - 1 / 2 });
+    return point.x < centerPoint.x && (isAboveLine(point, line) || isAtLine(point, line));
 }
 
-const partitionFilters = [partion0Filter, partion1Filter, partion2Filter, partion3Filter, partion4Filter, partion5Filter, partion6Filter, partion6Filter, partion7Filter];
+const partitionFilters = [partion0Filter, partion1Filter, partion2Filter, partion3Filter, partion4Filter, partion5Filter, partion6Filter, partion7Filter];
 
 function getDistanceByPoints(point1, point2) {
-    return Math.sqrt(Math.pow(point1.col - point2.col, 2) + Math.pow(point1.row - point2.row, 2));
+    return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
 }
 
 function getClosestFromSet(point, filter, objects) {
     let centerObjs = objects
-        .map(p => ({ row: p.row + 1 / 2, col: p.col + 1 / 2 }))
-        .filter(filter);
+        .map(p => ({ y: p.y + 1 / 2, x: p.x + 1 / 2 }))
+        .filter(p => filter(point, p));
     let mappedToDistance = centerObjs.map(elem => getDistanceByPoints(point, elem));
-    return Math.min(...mappedToDistance);
+    return mappedToDistance.length ? Math.min(...mappedToDistance) : -1;
 }
 
 export function getNeuronInputForPoint(point, apples, walls, bodies) {
-    let bodiesWithoutCurrent = bodies.filter(elem => elem.row != point.row || elem.col != point.col);
-    let centerPoint = { row: point.row + 1 / 2, col: point.col + 1 / 2 };
+    let bodiesWithoutCurrent = bodies.filter(elem => elem.y != point.y || elem.x != point.x);
+    let centerPoint = { y: point.y + 1 / 2, x: point.x + 1 / 2 };
     let neuronInput = [];
     let objects = [apples, walls, bodiesWithoutCurrent];
     for (let i = 0; i < 8; i++) {
