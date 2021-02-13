16b0a6c88ce05f978bcf154dc405ee8764cc9560
Hristiyana Panayotova <hpanayotova@vmware.com>
2021-02-13 19:41:22 +0200

    add mutation

--- js/GeneticAlgorithm/Mutation.js
+++ js/GeneticAlgorithm/Mutation.js
@@ -1,4 +1,12 @@
-export function mutation(snake, mutationRate) {
-
-
+//uniform mutation
+function mutation(snake, mutationRate) {
+    let config = snake.getSnakeConfig();
+    for (let layer of config) {
+        for (let neuron of layer) {
+            if (Math.random() < mutationRate) neuron.bias = NeuronValueModule.getRandomValue();
+            neuron.weights.forEach((_, i) => {
+                if (Math.random() < mutmutationRateationPercent) neuron.weights[i] = NeuronValueModule.getRandomValue();
+            });
+        }
+    }
 }
